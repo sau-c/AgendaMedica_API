01309d0849ab4c2b4638155886c7320aeeea39df
sau-c <81703475+sau-c@users.noreply.github.com>
2026-03-19 00:59:39 -0600

    Commit

--- src/AgendaMedica.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AgendaMedica.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
